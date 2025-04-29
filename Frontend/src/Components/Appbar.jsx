9db3e32945af8476b74268547b0c03e572738ca5
Shardul Kolte <shardulkolte8@gmail.com>
2025-04-29 14:58:49 +0530

    appbar profile pic set

--- Frontend/src/Components/Appbar.jsx
+++ Frontend/src/Components/Appbar.jsx
@@ -1,10 +1,48 @@
 import { faker } from "@faker-js/faker";
 import { ChatBubbleOutline } from "@mui/icons-material";
 import { AppBar, Avatar, IconButton, Toolbar } from "@mui/material";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Appbar = () => {
+  const [user, setUser] = useState(null);
+  const [formData, setFormData] = useState({
+      profilePic: "",
+    });
+
+
+    useEffect(() => {
+      const fetchProfileAndPosts = async () => {
+        try {
+          const token = localStorage.getItem("token");
+  
+          if (!token) {
+            console.error("No token found!");
+            return;
+          }
+  
+          // Fetch profile
+          const profileRes = await axios.get(
+            "http://localhost:5000/api/profile/me",
+            {
+              headers: { Authorization: `Bearer ${token}` },
+            }
+          );
+  
+          setUser(profileRes.data);
+  
+          setFormData({
+            profilePic: profileRes.data.profilePic || "",
+          });
+  
+        } catch (err) {
+          console.error("Failed to fetch profile or posts:", err);
+        }
+      };
+  
+      fetchProfileAndPosts();
+    }, []);
   return (
     <AppBar position="fixed" sx={{ background: "rgba(22, 27, 34, 0.9)" }}>
       <Toolbar sx={{ justifyContent: "flex-end" }}>
@@ -12,7 +50,7 @@ const Appbar = () => {
           <ChatBubbleOutline />
         </IconButton>
         <Link to={"/profile"}>
-          <Avatar src={faker.image.avatar()} />
+          <Avatar src={formData.profilePic || "/images/default-avatar.png"} />
         </Link>
       </Toolbar>
     </AppBar>
